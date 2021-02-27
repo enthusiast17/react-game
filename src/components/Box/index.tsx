e4b6b2b4dfa5eb6a05364aabc9cdd3a216d503cd
enthusiast17 <ulan17nurym@gmail.com>
2021-02-27 19:54:16 +0500

    feat: add box

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { IBox } from '../../interfaces';
+import './index.scss';
+
+const Box = (props: IBox) => {
+  return (
+    <div className="box" />
+  )
+}
+
+export default Box;
