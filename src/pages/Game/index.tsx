ca6820028a621cb0e6a5bd2414e42cd2fae30c0d
enthusiast17 <ulan17nurym@gmail.com>
2021-02-27 19:54:31 +0500

    feat: add game

---
+++ src/pages/Game/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Board from '../../components/Board';
+import './index.scss';
+
+const Game = () => (
+  <div className="game">
+    <Board />
+  </div>
+);
+
+export default Game;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Game from '../Game';
 import './index.scss';
 
-const Home = () => (
-  <div className="home">
-    <div className="content">
-      <h1 className="title">Show the path -&gt;</h1>
-      <div className="btns">
-        <button className="btn btn-outline-success">Play game</button>
-        <button className="btn btn-outline-primary">Score board</button>
-        <button className="btn btn-outline-primary">Guide</button>
-        <button className="btn btn-outline-primary">Settings</button>
-      </div>
-    </div>
-  </div>
-);
+const Home = () => {
+  const [playGame, setPlayGame] = useState<boolean>(false);
+
+  return (
+    <>
+      {!playGame && (
+        <div className="home">
+          <div className="content">
+            <h1 className="title">Show the path -&gt;</h1>
+            <div className="btns">
+              <button className="btn btn-outline-success" onClick={() => setPlayGame(true)}>Play game</button>
+              <button className="btn btn-outline-primary">Score board</button>
+              <button className="btn btn-outline-primary">Guide</button>
+              <button className="btn btn-outline-primary">Settings</button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {playGame && (<Game />)}
+    </>
+  );
+}
 
 export default Home;
