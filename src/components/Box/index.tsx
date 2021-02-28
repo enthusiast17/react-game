091ab3bf0b46eeaa07a1fb287766d8a534c9c687
enthusiast17 <ulan17nurym@gmail.com>
2021-02-28 12:49:45 +0500

    feat: add coin

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -3,10 +3,11 @@ import { useSelector } from 'react-redux';
 import { IBox, ICoordinate } from '../../interfaces';
 import { RootState } from '../../store';
 import Arrow from '../Arrow';
+import coinImg from '../../assets/coin.png';
 import './index.scss';
 
 const Box = (props: IBox) => {
-  const { coordinate } = props;
+  const { coordinate, hasCoin } = props;
   const finishCoordinate = useSelector((state: RootState) => state.board.finishCoordinate);
   const arrowCoordinate = useSelector((state: RootState) => state.arrow.coordinate);
 
@@ -18,6 +19,10 @@ const Box = (props: IBox) => {
     <div className={isCoordinatesEqual(coordinate, finishCoordinate) ? 'box is-finish' : 'box'}>
       {isCoordinatesEqual(coordinate, arrowCoordinate) && (
         <Arrow />
+      )}
+
+      {hasCoin && (
+        <img src={coinImg} alt="coin" />
       )}
     </div>
   )
