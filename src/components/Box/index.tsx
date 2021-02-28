6be20ec9887a747ff98fb7147b04368f4be3a06d
enthusiast17 <ulan17nurym@gmail.com>
2021-02-28 12:19:49 +0500

    feat: add is-finish box

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { IBox } from '../../interfaces';
+import { IBox, ICoordinate } from '../../interfaces';
 import { RootState } from '../../store';
 import Arrow from '../Arrow';
 import './index.scss';
 
 const Box = (props: IBox) => {
-  const { coodinate } = props;
+  const { coordinate } = props;
+  const finishCoordinate = useSelector((state: RootState) => state.board.finishCoordinate);
   const arrowCoordinate = useSelector((state: RootState) => state.arrow.coordinate);
 
+  const isCoordinatesEqual = (firstCoordinate: ICoordinate, secondCoordinate: ICoordinate) => (
+    JSON.stringify(firstCoordinate) === JSON.stringify(secondCoordinate)
+  );
+
   return (
-    <div className="box">
-      {JSON.stringify(coodinate) === JSON.stringify(arrowCoordinate) && (
+    <div className={isCoordinatesEqual(coordinate, finishCoordinate) ? 'box is-finish' : 'box'}>
+      {isCoordinatesEqual(coordinate, arrowCoordinate) && (
         <Arrow />
       )}
     </div>
