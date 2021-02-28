71004ca6dc0913162ce3e3d95250ca85130906d7
enthusiast17 <ulan17nurym@gmail.com>
2021-02-28 12:18:20 +0500

    refactor: make column flex in game scss

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Board from '../../components/Board';
 import './index.scss';
 
-const Game = () => (
-  <div className="game">
-    <Board />
-  </div>
-);
+const Game = () => {
+  return (
+    <div className="game">
+      <Board />
+    </div>
+  );
+}
 
 export default Game;
